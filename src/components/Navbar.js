65d57400143100a85dadb12b0f7e5367e3066df6
Chanchal <96549592+kmChanchal@users.noreply.github.com>
2025-12-15 20:25:28 +0530

    Update GitHub link in Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -103,7 +103,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/kmChanchal"
                 target="_blank"
                 className="fork-btn-inner"
               >
